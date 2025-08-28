bcb649073e02e11422becdd2f6be89da79c1475a
Neha <naiharana173@gmail.com>
2025-08-28 05:04:10 -0700

    initial commit

--- src/components/Topbar.jsx
+++ src/components/Topbar.jsx
@@ -1,22 +1,3 @@
-// import React from "react";
-// import { User } from "lucide-react";
-
-// export default function Topbar() {
-//   return (
-//     <div className="flex items-center justify-between bg-white p-4 border-b">
-//       <div className="text-xl font-bold  text-purple-700">EventHub</div>
-//       <div className="flex items-center gap-2">
-//         <User className="h-6 w-6 text-blue-300" />
-//       </div>
-//     </div>
-
-  
-
-//   );
-// }
-
-
-
 import React from "react";
 import { User } from "lucide-react";
 import { Button } from "@/components/ui/button";
